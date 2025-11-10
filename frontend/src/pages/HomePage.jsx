cc33a4027865a5e11b73ee4cf791ebce751a56df
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-11-10 21:04:31 +0100

    Add: setup for tailwindCSS, shadcn and ui for NotFound page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return <div><h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1></div>;
 }
 
 export default HomePage;
